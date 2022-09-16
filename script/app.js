be1aa6f8fd261f86ca2a0c5903419b0ffb326e48
p4riah <82164005+d4r14h@users.noreply.github.com>
2022-09-16 11:30:15 -0400

    Uploaded site files

---
+++ script/app.js
@@ -0,0 +1,62 @@
+/*
+ * ASCII Camera
+ * http://idevelop.github.com/ascii-camera/
+ *
+ * Copyright 2013, Andrei Gheorghe (http://github.com/idevelop)
+ * Released under the MIT license
+ */
+
+(function() {
+	var asciiContainer = document.getElementById("ascii");
+	var capturing = false;
+
+	camera.init({
+		width: 160,
+		height: 120,
+		fps: 30,
+		mirror: true,
+
+		onFrame: function(canvas) {
+			ascii.fromCanvas(canvas, {
+				// contrast: 128,
+				callback: function(asciiString) {
+					asciiContainer.innerHTML = asciiString;
+				}
+			});
+		},
+
+		onSuccess: function() {
+			document.getElementById("info").style.display = "none";
+
+			const button = document.getElementById("button");
+			button.style.display = "block";
+			button.onclick = function() {
+				if (capturing) {
+					camera.pause();
+					button.innerText = 'resume';
+				} else {
+					camera.start();
+					button.innerText = 'pause';
+				}
+				capturing = !capturing;
+			};
+
+			/* const asciicaptbtn = document.getElementById("txtcapture");
+			asciicaptbtn.style.display = "block";
+			asciicaptbtn.onclick = function() {
+				var blob = new Blob([asciiString], { type: "text/plain;charset=utf-8" });
+				saveAs(blob, "asciicapture.txt");
+			}; */
+		},
+
+		onError: function(error) {
+			// TODO: log error
+		},
+
+		onNotSupported: function() {
+			document.getElementById("info").style.display = "none";
+			asciiContainer.style.display = "none";
+			document.getElementById("notSupported").style.display = "block";
+		}
+	});
+})();
